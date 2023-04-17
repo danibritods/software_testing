56b5d18f102ecd18a20f1d7e828f88863c053c41
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-17 10:56:55 -0300

    feat: add test to q1

---
+++ exercise_list_1/tests/test_exercise_list_1.py
@@ -0,0 +1,20 @@
+import pytest
+from exercise_list_1 import q1, q2
+
+
+def test_q1( monkeypatch, capsys):
+    in_text = "test_string"
+    out_text = "test_string"
+    helper_assert_in_out(q1.main, in_text, out_text, monkeypatch, capsys)
+
+def helper_assert_in_out(function, in_text, out_text, monkeypatch, capsys):
+    # Simulate user input
+    monkeypatch.setattr("builtins.input", lambda _: f"{in_text}")
+
+    # Call function that prints something
+    function()
+
+    # Check if output is correct
+    captured = capsys.readouterr()
+    assert captured.out == f"{out_text}\n"
+
