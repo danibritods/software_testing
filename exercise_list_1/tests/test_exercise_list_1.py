37b2a33a0d056dd54956e2a3d573ae2cc42323c9
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-17 11:00:05 -0300

    refactor: put helper first

--- exercise_list_1/tests/test_exercise_list_1.py
+++ exercise_list_1/tests/test_exercise_list_1.py
@@ -1,12 +1,6 @@
 import pytest
 from exercise_list_1 import q1, q2
 
-
-def test_q1( monkeypatch, capsys):
-    in_text = "test_string"
-    out_text = "test_string"
-    helper_assert_in_out(q1.main, in_text, out_text, monkeypatch, capsys)
-
 def helper_assert_in_out(function, in_text, out_text, monkeypatch, capsys):
     # Simulate user input
     monkeypatch.setattr("builtins.input", lambda _: f"{in_text}")
@@ -17,4 +11,10 @@ def helper_assert_in_out(function, in_text, out_text, monkeypatch, capsys):
     # Check if output is correct
     captured = capsys.readouterr()
     assert captured.out == f"{out_text}\n"
+
+def test_q1( monkeypatch, capsys):
+    in_text = "test_string"
+    out_text = "test_string"
+    helper_assert_in_out(q1.main, in_text, out_text, monkeypatch, capsys)
+
 
