a60ce76bdb7471ccbfce2fc12c4d53a36611b2ea
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-17 11:13:11 -0300

    refactor: separate the input and output mocking

--- exercise_list_1/tests/test_exercise_list_1.py
+++ exercise_list_1/tests/test_exercise_list_1.py
@@ -1,20 +1,20 @@
 import pytest
 from exercise_list_1 import q1, q2
 
-def helper_assert_in_out(function, in_text, out_text, monkeypatch, capsys):
-    # Simulate user input
-    monkeypatch.setattr("builtins.input", lambda _: f"{in_text}")
 
-    # Call function that prints something
-    function()
+def enter_input(input_text, monkeypatch):
+    monkeypatch.setattr("builtins.input", lambda _: f"{input_text}")
 
-    # Check if output is correct
-    captured = capsys.readouterr()
-    assert captured.out == f"{out_text}\n"
 
-def test_q1( monkeypatch, capsys):
+def capture_print(capsys):
+    captured_text = capsys.readouterr().out
+    return captured_text
+
+
+def test_q1(monkeypatch, capsys):
     in_text = "test_string"
-    out_text = "test_string"
-    helper_assert_in_out(q1.main, in_text, out_text, monkeypatch, capsys)
-
-
+    out_text = "test_string\n"
+    enter_input(in_text, monkeypatch)
+    q1.main()
+    captured = capture_print(capsys)
+    assert out_text == captured
