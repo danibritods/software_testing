543e31deaf8e22bf8d771831939031a9a83ad8a4
Daniel Brito <danielbrito@pq.uenf.br>
2023-04-18 18:16:15 -0300

    feat: add another version of q5

--- exercise_list_1/q6.py
+++ exercise_list_1/q6.py
@@ -20,3 +20,21 @@ def read_names_return_youngest_n_oldest() -> str:
             youngest["name"] = input_name
     result_string: str = f"youngest: {youngest['name']}:{youngest['age']}, oldest: {oldest['name']}:{oldest['age']}."
     print(result_string)
+
+
+def read_names_return_youngest_n_oldest_tuples() -> None:
+    name_age: List[Tuple[str, int]] = []
+    while True:
+        input_name: str = input("Enter a name: ")
+        if input_name == "-1":
+            break
+        input_age: int = int(input(f"Enter {input_name}'s age: "))
+        if input_age < 0:
+            break
+        name_age.append((input_name, input_age))
+    oldest = max(name_age, key=lambda t: t[1])
+    youngest = min(name_age, key=lambda t: t[1])
+    result_string: str = (
+        f"youngest: {youngest[0]}:{youngest[1]}, oldest: {oldest[0]}:{oldest[1]}."
+    )
+    print(result_string)
